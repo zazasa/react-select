7288c88d918859b699704e0cf2ef3121e643f22a
Charles Lee <cc.lee@live.com.au>
2018-05-02 11:46:27 +1000

    aria hidden true on indicators and svgs

--- src/Select.js
+++ src/Select.js
@@ -1240,7 +1240,7 @@ export default class Select extends Component<Props, State> {
     if (!LoadingIndicator || !isLoading) return null;
 
     const innerProps = {
-      role: 'presentation',
+      'aria-hidden': 'true',
     };
 
     return (
